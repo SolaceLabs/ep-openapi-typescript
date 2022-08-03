3a4d25710f02ec3c12482c33905cde8a4d6fdbfc
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2022-08-03 13:15:14 +0200

    tests

---
+++ ep-openapi/test/source.env.sh
@@ -0,0 +1,27 @@
+#
+# Environment for tests
+#
+# Usage:
+#   source source.env.sh && {run-tests} && unset_source_env
+
+unset_source_env() {
+    # env vars for tests
+    unset EP_OPENAPI_TEST_EP_API_BASE_URL
+
+    # unset this function
+    unset -f unset_source_env
+}
+
+export EP_OPENAPI_TEST_EP_API_BASE_URL="https://ian-dev-api.mymaas.net"
+
+######################################################
+
+NOLOG_EP_OPENAPI_TEST_SOLACE_CLOUD_TOKEN=$EP_OPENAPI_TEST_SOLACE_CLOUD_TOKEN
+export EP_OPENAPI_TEST_SOLACE_CLOUD_TOKEN="***"
+
+logName='[source.env.sh]'
+echo "$logName - test environment:"
+echo "$logName - EP_OPENAPI_TEST:"
+export -p | sed 's/declare -x //' | grep EP_OPENAPI_TEST_
+
+export EP_OPENAPI_TEST_SOLACE_CLOUD_TOKEN=$NOLOG_EP_OPENAPI_TEST_SOLACE_CLOUD_TOKEN
